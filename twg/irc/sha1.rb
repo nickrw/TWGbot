b15b778b643b92f1eba21a962b7f3b243c7aded9
Nicholas Robinson-Wall <nick@robinson-wall.com>
2013-01-24 16:11:13 +0000

    Retain sha1 state at start, and compare to current.

--- twg/irc/sha1.rb
+++ twg/irc/sha1.rb
@@ -6,22 +6,43 @@ module TWG
 
       def initialize(*args)
         super
+        @pwd = File.expand_path File.dirname(__FILE__)
+        @isha1 = `cd #{@pwd} && git rev-parse HEAD`.strip
+        @ichanges = `cd #{@pwd} && git status --porcelain`.strip
       end
 
       def sha1(m)
 
         return if !m.channel?
 
-        pwd = File.expand_path File.dirname(__FILE__)
-        sha1 = `cd #{pwd} && git rev-parse HEAD`.strip
-        changes = `cd #{pwd} && git status --porcelain`.strip
+        sha1 = `cd #{@pwd} && git rev-parse HEAD`.strip
+        changes = `cd #{@pwd} && git status --porcelain`.strip
 
-        m.reply "Commit: #{sha1}"
-        if changes.empty?
-          m.reply "No working copy changes"
+        if sha1 != @isha1
+          m.reply "!! I was started with a different commit to the current HEAD"
+          m.reply "!! Running commit: #{@isha1}"
+          m.reply "!! Current commit: #{sha1}"
         else
-          m.reply "The following changes are present in my working copy"
-          m.reply changes
+          m.reply "Running Commit: #{sha1}"
+        end
+
+        if changes != @ichanges
+          m.reply describe_changes(@ichanges, 'Running working copy')
+          m.reply describe_changes(changes, 'Current working copy')
+        else 
+          m.reply describe_changes(changes)
+        end
+      end
+
+      private
+
+      def describe_changes(changes, type='')
+        type = (type.empty? ? '' : type + ': ')
+        if changes.empty?
+          type + "No working copy changes"
+        else
+          type + "Changes in the working copy:\n" + changes
+
         end
       end
 
