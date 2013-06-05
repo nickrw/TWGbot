33a063f68ea367a2617661c42b67045297ba8cbb
Nicholas Robinson-Wall <nick@robinson-wall.com>
2013-06-05 21:28:31 +0100

    Return immediately in seer dispatch methods if there is no seer

--- twg/irc/seer.rb
+++ twg/irc/seer.rb
@@ -14,33 +14,37 @@ module TWG
   
       def solicit_seer_choice(m)
         return if shared[:game].nil?
-        if shared[:game].seer && shared[:game].participants[shared[:game].seer] != :dead
+        return if shared[:game].seer.nil?
+        if shared[:game].participants[shared[:game].seer] != :dead
           User(shared[:game].seer).send "It is now NIGHT %s: say !see <nickname> to me to reveal their role." % shared[:game].iteration.to_s
         end
       end
       
       def seer_reveal(m, r = nil)
         return if shared[:game].nil?
-        unless r.nil?
-          case r.code
-            when :targetkilled
-              User(shared[:game].seer).send "You have a vision of %s's body, twisted, broken and bloody. A wolf got there before you." % r.opts[:target]
-            when :youkilled
-              User(shared[:game].seer).send "While dreaming vividly about %s you get viciously torn to pieces. Your magic 8 ball didn't see that one coming!" % r.opts[:target]
-            when :sawwolf
-              User(shared[:game].seer).send "You have a vivid dream about %s wearing a new fur coat. You've found a WOLF." % r.opts[:target]
-            when :sawhuman
-              User(shared[:game].seer).send "You have a dull dream about %s lying in bed, dreaming. It looks like %s is a fellow villager." % [r.opts[:target], r.opts[:target]]
-          end
+        return if shared[:game].seer.nil?
+        return if r.nil?
+        case r.code
+          when :targetkilled
+            User(shared[:game].seer).send "You have a vision of %s's body, twisted, broken and bloody. A wolf got there before you." % r.opts[:target]
+          when :youkilled
+            User(shared[:game].seer).send "While dreaming vividly about %s you get viciously torn to pieces. Your magic 8 ball didn't see that one coming!" % r.opts[:target]
+          when :sawwolf
+            User(shared[:game].seer).send "You have a vivid dream about %s wearing a new fur coat. You've found a WOLF." % r.opts[:target]
+          when :sawhuman
+            User(shared[:game].seer).send "You have a dull dream about %s lying in bed, dreaming. It looks like %s is a fellow villager." % [r.opts[:target], r.opts[:target]]
         end
       end
   
       def notify_roles(m)
         return if shared[:game].nil?
+        return if shared[:game].seer.nil?
         User(shared[:game].seer).send "You are the SEER. Each night you can have the role of a player of your choice revealed. Choose carefully, once the inner eye has selected a target it cannot be swayed." if shared[:game].seer
       end
   
       def see(m, target)
+        return if shared[:game].nil?
+        return if shared[:game].seer.nil?
         if shared[:game] && m.channel? == false
           r = shared[:game].see(m.user.nick, target)
           case r.code
