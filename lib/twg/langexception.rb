70964702bcceb92d7adde28ee98ff341a4eb915e
Nicholas Robinson-Wall <nick@robinson-wall.com>
2013-07-21 01:14:35 +0100

    Fix LangException not ignoring the special i18n.plural.rule key

--- lib/twg/langexception.rb
+++ lib/twg/langexception.rb
@@ -3,7 +3,7 @@ require 'i18n'
 module TWG
   class LangException < I18n::ExceptionHandler
     def call(exception, locale, key, options)
-      if exception.is_a?(I18n::MissingTranslation)
+      if exception.is_a?(I18n::MissingTranslation) && key.to_s != 'i18n.plural.rule'
         raise exception.to_exception
       else
         super
