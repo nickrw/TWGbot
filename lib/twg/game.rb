2817107fd2c559932dddf01633a370a5a26e04fa
Nicholas Robinson-Wall <nick@robinson-wall.com>
2014-05-14 11:33:43 +0100

    Remove conversion of TWG::Game participants list to strings
    These will be Cinch::User objects, which will update names automatically
    and be case insensitive when they are searched for.

--- lib/twg/game.rb
+++ lib/twg/game.rb
@@ -213,12 +213,6 @@ module TWG
 
     def assign_roles
 
-      # Convert all keys in @participants to strings, to make comparison (voting, etc) easier.
-      # These will initially be Cinch::User objects for IRC.
-      partasstring = {}
-      @participants.keys.each { |part| partasstring[part.to_s] = :normal }
-      @participants = partasstring
-
       @live_wolves = (@wolf_ratio * @participants.length).to_i
       @live_norms = @participants.length - @live_wolves
       @game_wolves = @participants.keys.shuffle[0..(@live_wolves-1)].sort
